d6ad42ec2884d0b530ff9076c504dc81c1012cb4
tm2 <tm2@c529fa20-d566-0410-9e6c-f764a1a7f490>
2009-04-10 19:52:02 +0000

    Some DES crypt stuff for NCD protocol

--- src/classes/clsCrypt.vb
+++ src/classes/clsCrypt.vb
@@ -21,6 +21,7 @@
 
 Imports System.Security.Cryptography
 Imports System.Text
+Imports System.IO
 
 Public Class clsAESCrypt
 
@@ -53,5 +54,69 @@ Public Class clsAESCrypt
             Encrypt = resultArray
         End Using
     End Function
+
+
+
+    'Hack: Some DES Stuff from MSDN and a good link: http://www.codeproject.com/KB/security/Crypto.aspx
+    Private oKey As New DESCryptoServiceProvider
+
+    Public Sub InitNCDKey(ByVal key() As Byte)
+        oKey.Key = New Byte() {&H1, &H2, &H3, &H4, &H5, &H6, &H7, &H8, &H9, &H10, &H11, &H12, &H13, &H14}
+    End Sub
+    ' Encrypt the string.
+    Public Function NewCamdEncrypt(ByVal PlainText As String) As Byte()
+        ' Create a memory stream.
+        Dim ms As New MemoryStream()
+
+        ' Create a CryptoStream using the memory stream and the 
+        ' CSP DES key.  
+        Dim encStream As New CryptoStream(ms, oKey.CreateEncryptor(), CryptoStreamMode.Write)
+
+        ' Create a StreamWriter to write a string
+        ' to the stream.
+        Dim sw As New StreamWriter(encStream)
+
+        ' Write the plaintext to the stream.
+        sw.WriteLine(PlainText)
+
+        ' Close the StreamWriter and CryptoStream.
+        sw.Close()
+        encStream.Close()
+
+        ' Get an array of bytes that represents
+        ' the memory stream.
+        Dim buffer As Byte() = ms.ToArray()
+
+        ' Close the memory stream.
+        ms.Close()
+
+        ' Return the encrypted byte array.
+        Return buffer
+    End Function 'Encrypt
+
+
+    ' Decrypt the byte array.
+    Public Function NewCamdDecrypt(ByVal CypherText() As Byte) As String
+        ' Create a memory stream to the passed buffer.
+        Dim ms As New MemoryStream(CypherText)
+
+        ' Create a CryptoStream using the memory stream and the 
+        ' CSP DES key. 
+        Dim encStream As New CryptoStream(ms, oKey.CreateDecryptor(), CryptoStreamMode.Read)
+
+        ' Create a StreamReader for reading the stream.
+        Dim sr As New StreamReader(encStream)
+
+        ' Read the stream as a string.
+        Dim val As String = sr.ReadLine()
+
+        ' Close the streams.
+        sr.Close()
+        encStream.Close()
+        ms.Close()
+
+        Return val
+    End Function 'Decrypt
+
 
 End Class
